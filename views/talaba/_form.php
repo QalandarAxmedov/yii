8e04c426dc582389c324ec3f1678e872223a96d8
AQDEV <uzwebdevolopersn2@gmail.com>
2021-10-05 00:19:40 -0400

    new commit

--- views/talaba/_form.php
+++ views/talaba/_form.php
@@ -14,22 +14,14 @@ use yii\widgets\ActiveForm;
 
 <div class="talaba-form">
 
-    <?php $form = ActiveForm::begin([
-    'id' => 'login-form',
-    'options' => ['class' => 'form-horizontal'],
-]);?>
-<div class="form-group">
-        <div class="col-lg-offset-1 col-lg-11">
+    <?php $form = ActiveForm::begin();?>
 
     <?=$form->field($model, 'ism')->textInput(['maxlength' => true, 'placeholder' => "Ismingizni kiriting"])->label(false)?>
 
-    <?=$form->field($model, 'familiya')->textInput(['maxlength' => true, 'disabled' => true])?>
+    <?=$form->field($model, 'familiya')->textInput(['maxlength' => true, 'placeholder' => "Familiyangizni kiriting"])?>
 
     <?=$form->field($model, 'otasining_ismi')->textInput(['maxlength' => true])?>
 
-            <!-- <?=Html::submitButton('Login', ['class' => 'btn btn-primary'])?> -->
-        </div>
-    </div>
     <?=$form->field($model, 'region_id')->dropDownList(
     ArrayHelper::map(Region::find()->all(), 'id', 'name'),
     [
@@ -38,7 +30,7 @@ use yii\widgets\ActiveForm;
             $.post( "' . urldecode(Yii::$app->urlManager->createUrl('district/lists?id=')) . '"+$(this).val()+"&faculty_id=", function( data ) {
               $( "#talaba-district_id" ).html( data );
             });
-        '])->label("Hudud");?>
+        ', ])->label("Hudud");?>
 
 
     <?=$form->field($model, 'district_id')->dropDownList(
